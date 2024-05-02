1ee5ce02f3bba4b12baced196000683f8c16a1f4
Joe Bowers <Joe.bowers@brandaddition.com>
2024-05-02 15:41:34 +0100

    commit from old laptop

--- detectscript.ps1
+++ detectscript.ps1
@@ -1,1 +1,11 @@
-exit 0
+$driver = get-printerdriver | where-object {$_.Name -eq "Canon Generic PCL6 Driver"}
+if ($driver -eq $nul)
+{
+    write-host "driver is nul"
+    exit 1
+}
+else
+{
+    write-host "driver is not nul"
+    exit 0
+}
